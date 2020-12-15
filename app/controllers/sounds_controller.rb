1e1a603db4de9e74c9f60689f605353d363a874a
Dmondial <www.ezweb.1ocofrank.acidman@gmail.com>
2020-12-15 16:35:42 +0900

    投稿機能実装　create可能でデータベースに保存されていることを確認

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -3,5 +3,21 @@ class SoundsController < ApplicationController
   end
 
   def new
+    @sound = Sound.new
+  end
+
+  def create
+    @sound = Sound.new(sound_params)
+    if @sound.save
+      redirect_to root_path
+    else
+      render new_sound_path
+    end
+  end
+
+  private
+
+  def sound_params
+    params.require(:sound).permit(:title, :explain, :audio).merge(user_id: current_user.id)
   end
 end
