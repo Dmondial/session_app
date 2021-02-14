01000ee750d3b80cb17d3c01f420a2646fb56012
Dmondial <www.ezweb.1ocofrank.acidman@gmail.com>
2021-02-15 00:17:29 +0900

    mypageにsession一覧表示を実装

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,5 +2,7 @@ class MypagesController < ApplicationController
   def index
     @user = User.find(current_user[:id])
     @sound = Sound.where(user_id: current_user[:id])
+    @session = Session.all
+    @session_sound = SessionSound.all
   end
 end
